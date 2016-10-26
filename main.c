5195e031c8927fb43f44f5a5afe497dfa856bafb
jerahmeelchua@WinMac <jzcxw13@gmail.com>
2016-10-26 09:49:57 +0800

    Test Commit

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ int main(char *argv[], int argc) {
 	printf("Welcome to the ICT1002 Logo interpreter.\n");
 	printf("Type commands one line at a time, after the ? prompt.\n");
 	printf("Execute EXIT to finish.\n\n");
-	
+	// Test
 	/* main interpreter loop */
 	while (!done) {
 	
